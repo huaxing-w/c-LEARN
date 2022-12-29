14ba2b80255341d9394da22d04676ee561989e0f
Huaxing <hwang68@syr.edu>
2022-12-29 13:42:55 -0800

    __uint128_t

---
+++ datatype/uint128.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main(){
+    __uint128_t a=1;
+    bitset<20> b=1;
+    cout<<b<<endl;
+    b>>=1;
+    cout<<b<<endl;
+
+    b<<=1;
+    cout<<b<<endl;
+    
+
+
+
+    return 0;
+}
--- network/socketProgrammingNew/bigEndianLittleEndian.c
+++ network/socketProgrammingNew/bigEndianLittleEndian.c
@@ -9,6 +9,8 @@
 // s: short
 // l: int
 
+//this only works int to int, in the socket programming, we use string to represent ipv4 and ipv6
+
 // 这套api主要用于 网络通信过程中 IP 和 端口 的 转换
 // 将一个短整形从主机字节序 -> 网络字节序
 uint16_t htons(uint16_t hostshort);	
@@ -21,8 +23,16 @@ uint16_t ntohs(uint16_t netshort);
 uint32_t ntohl(uint32_t netlong);
 
 
+
 int main(){
     int a = 0x12345678;
     printf("%d\n",htons(a));
+
+    const char* ip4="192.168.33.10";
+    printf("%s\n",ip4);
+
+
+
+
     return 0;
 }
